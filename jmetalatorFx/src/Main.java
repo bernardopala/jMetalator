cd2e9dbd69bf8462564db075630494eeee6b9cbc
bernard_opala <bernard_opala@bitbucket.org>
2019-07-06 12:14:59 +0200

    Added unification of quality indicators value format.

--- jmetalatorFx/src/Main.java
+++ jmetalatorFx/src/Main.java
@@ -1,127 +1,68 @@
 import com.orsoncharts.Chart3D;
-import com.orsoncharts.Chart3DFactory;
-import com.orsoncharts.Chart3DPanel;
 import com.orsoncharts.Colors;
-import com.orsoncharts.axis.LabelOrientation;
 import com.orsoncharts.axis.NumberAxis3D;
 import com.orsoncharts.data.xyz.XYZDataItem;
-import com.orsoncharts.data.xyz.XYZDataset;
 import com.orsoncharts.data.xyz.XYZSeries;
 import com.orsoncharts.data.xyz.XYZSeriesCollection;
 import com.orsoncharts.fx.Chart3DViewer;
-import com.orsoncharts.graphics3d.Dimension3D;
 import com.orsoncharts.graphics3d.ViewPoint3D;
-import com.orsoncharts.graphics3d.swing.DisplayPanel3D;
-import com.orsoncharts.graphics3d.swing.ZoomInAction;
-import com.orsoncharts.label.StandardXYZLabelGenerator;
 import com.orsoncharts.plot.XYZPlot;
 import com.orsoncharts.renderer.xyz.ScatterXYZRenderer;
-import com.orsoncharts.renderer.xyz.XYZRenderer;
 import com.orsoncharts.style.ChartStyles;
 import javafx.application.Platform;
 import javafx.beans.property.*;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.ScatterChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import javafx.util.Callback;
 import javafx.util.Pair;
 import jmetalhelpers.ExperimentParams;
-import jmetalhelpers.QualityIndicator;
 import jmetalhelpers.SolutionDto;
 import jmetalhelpers.algorithms.AlgorithmParameter;
-import jmetalhelpers.algorithms.NSGAIIManager;
-import jmetalhelpers.algorithms.SPEA2Manager;
-import maths.EvenlyDistributedSolutions;
 import maths.GenLloyd;
-import maths.GoodDistribution;
-import org.apache.commons.lang3.SerializationUtils;
-import org.apache.commons.lang3.tuple.ImmutableTriple;
-import org.apache.commons.lang3.tuple.Triple;
-import org.apache.commons.math3.geometry.partitioning.utilities.OrderedTuple;
 import org.jfree.chart.*;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.NumberTickUnit;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.block.BlockBorder;
 import org.jfree.chart.fx.ChartCanvas;
-import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.plot.Zoomable;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.chart.title.TextTitle;
-import org.jfree.chart.ui.HorizontalAlignment;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.time.Month;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.uma.jmetal.algorithm.Algorithm;
 import org.uma.jmetal.algorithm.impl.AbstractEvolutionaryAlgorithm;
-import org.uma.jmetal.algorithm.multiobjective.moead.util.MOEADUtils;
 import org.uma.jmetal.algorithm.util.CurrentSolutionSetReceiver;
-import org.uma.jmetal.problem.Problem;
 import org.uma.jmetal.qualityindicator.impl.*;
 import org.uma.jmetal.qualityindicator.impl.hypervolume.PISAHypervolume;
-import org.uma.jmetal.qualityindicator.impl.hypervolume.WFGHypervolume;
 import org.uma.jmetal.solution.DoubleSolution;
-import org.uma.jmetal.solution.Solution;
-import org.uma.jmetal.util.JMetalException;
-import org.uma.jmetal.util.ProblemUtils;
 import org.uma.jmetal.util.front.Front;
 import org.uma.jmetal.util.front.imp.ArrayFront;
 import org.uma.jmetal.util.front.util.FrontNormalizer;
 import org.uma.jmetal.util.front.util.FrontUtils;
 import org.uma.jmetal.util.point.util.PointSolution;
-import org.uma.jmetal.util.referencePoint.impl.IdealPoint;
 import view.PairKeyFactory;
 import view.PairValueCell;
 import view.PairValueFactory;
 import view.TabEnum;
 
-import javax.swing.text.TableView;
 import java.awt.*;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
-import static jdk.nashorn.internal.objects.NativeFunction.bind;
 
 public class Main implements CurrentSolutionSetReceiver<DoubleSolution>, Initializable {
 
@@ -273,13 +214,13 @@ public class Main implements CurrentSolutionSetReceiver<DoubleSolution>, Initial
     private final StringProperty selectedTab = new SimpleStringProperty();
     private final StringProperty qiResults = new SimpleStringProperty();
     private final SimpleIntegerProperty receivedSSProperty = new SimpleIntegerProperty();
-    private final SimpleDoubleProperty gdErrorProperty = new SimpleDoubleProperty();
-    private final SimpleDoubleProperty igdErrorProperty = new SimpleDoubleProperty();
-    private final SimpleDoubleProperty igdPlusErrorProperty = new SimpleDoubleProperty();
-    private final SimpleDoubleProperty spreadErrorProperty = new SimpleDoubleProperty();
-    private final SimpleDoubleProperty epsilonErrorProperty = new SimpleDoubleProperty();
-    private final SimpleDoubleProperty hvErrorProperty = new SimpleDoubleProperty();
-    private final SimpleDoubleProperty erErrorProperty = new SimpleDoubleProperty();
+    private final SimpleStringProperty gdErrorProperty = new SimpleStringProperty();
+    private final SimpleStringProperty igdErrorProperty = new SimpleStringProperty();
+    private final SimpleStringProperty igdPlusErrorProperty = new SimpleStringProperty();
+    private final SimpleStringProperty spreadErrorProperty = new SimpleStringProperty();
+    private final SimpleStringProperty epsilonErrorProperty = new SimpleStringProperty();
+    private final SimpleStringProperty hvErrorProperty = new SimpleStringProperty();
+    private final SimpleStringProperty erErrorProperty = new SimpleStringProperty();
 
     private final SimpleIntegerProperty is2dChartAutoResizing = new SimpleIntegerProperty();
     private final SimpleIntegerProperty isPCChartAutoResizing = new SimpleIntegerProperty();
@@ -367,6 +308,10 @@ public class Main implements CurrentSolutionSetReceiver<DoubleSolution>, Initial
     private List<Double> epsilonValues = new ArrayList<>();
     private List<Double> hvValues = new ArrayList<>();
     private List<Double> erValues = new ArrayList<>();
+    private DecimalFormat decimalFormat = new DecimalFormat("0.00000000");
+    private DecimalFormat decimalFormat2 = new DecimalFormat("0.0000E0");
+
+
 
     private final AtomicLong counter = new AtomicLong(-1);
 
@@ -618,13 +563,13 @@ public class Main implements CurrentSolutionSetReceiver<DoubleSolution>, Initial
 
         qiResultsLabel.textProperty().bind(qiResults);
         receivedSSLabel.textProperty().bind(receivedSSProperty.asString());
-        gdErrorValueLabel.textProperty().bind(gdErrorProperty.asString());
-        igdErrorValueLabel.textProperty().bind(igdErrorProperty.asString());
-        igdPlusErrorValueLabel.textProperty().bind(igdPlusErrorProperty.asString());
-        spreadErrorValueLabel.textProperty().bind(spreadErrorProperty.asString());
-        epsilonErrorValueLabel.textProperty().bind(epsilonErrorProperty.asString());
-        hvErrorValueLabel.textProperty().bind(hvErrorProperty.asString());
-        erErrorValueLabel.textProperty().bind(erErrorProperty.asString());
+        gdErrorValueLabel.textProperty().bind(gdErrorProperty);
+        igdErrorValueLabel.textProperty().bind(igdErrorProperty);
+        igdPlusErrorValueLabel.textProperty().bind(igdPlusErrorProperty);
+        spreadErrorValueLabel.textProperty().bind(spreadErrorProperty);
+        epsilonErrorValueLabel.textProperty().bind(epsilonErrorProperty);
+        hvErrorValueLabel.textProperty().bind(hvErrorProperty);
+        erErrorValueLabel.textProperty().bind(erErrorProperty);
         solutionsV1TableColumn.setCellValueFactory(r -> r.getValue().v1Property());
         solutionsV2TableColumn.setCellValueFactory(r -> r.getValue().v2Property());
         solutionsV3TableColumn.setCellValueFactory(r -> r.getValue().v3Property());
@@ -651,6 +596,14 @@ public class Main implements CurrentSolutionSetReceiver<DoubleSolution>, Initial
         epsilonCheckBox.setDisable(true);
         hvCheckBox.setDisable(true);
         erCheckBox.setDisable(true);
+
+        gdErrorProperty.setValue("0.0");
+        igdErrorProperty.setValue("0.0");
+        igdPlusErrorProperty.setValue("0.0");
+        spreadErrorProperty.setValue("0.0");
+        epsilonErrorProperty.setValue("0.0");
+        hvErrorProperty.setValue("0.0");
+        erErrorProperty.setValue("0.0");
 
         Object alg = (String)algorithmsComboBox.getItems().get(0);
         algorithmsComboBox.setValue(alg);
@@ -870,13 +823,13 @@ public class Main implements CurrentSolutionSetReceiver<DoubleSolution>, Initial
                 try {
                     lock.lock();
 
-                    gdErrorProperty.setValue(gd);
-                    igdErrorProperty.setValue(igd);
-                    igdPlusErrorProperty.setValue(igdPlus);
-                    spreadErrorProperty.setValue(spread);
-                    epsilonErrorProperty.setValue(epsilon);
-                    hvErrorProperty.setValue(hv);
-                    erErrorProperty.setValue(er);
+                    gdErrorProperty.setValue(decimalFormat.format(gd) + " (" + decimalFormat2.format(gd) + ")");
+                    igdErrorProperty.setValue(decimalFormat.format(igd) + " (" + decimalFormat2.format(igd) + ")");
+                    igdPlusErrorProperty.setValue(decimalFormat.format(igdPlus) + " (" + decimalFormat2.format(igdPlus) + ")");
+                    spreadErrorProperty.setValue(decimalFormat.format(spread) + " (" + decimalFormat2.format(spread) + ")");
+                    epsilonErrorProperty.setValue(decimalFormat.format(epsilon) + " (" + decimalFormat2.format(epsilon) + ")");
+                    hvErrorProperty.setValue(decimalFormat.format(hv) + " (" + decimalFormat2.format(hv) + ")");
+                    erErrorProperty.setValue(decimalFormat.format(er) + " (" + decimalFormat2.format(er) + ")");
                     receivedSSProperty.setValue(receiveSolutionSetCount);
 
                     if (selectedTab.getValue().equalsIgnoreCase("tab3d")) {
@@ -1329,10 +1282,13 @@ public class Main implements CurrentSolutionSetReceiver<DoubleSolution>, Initial
         stopButton.setDisable(false);
 
         receiveSolutionSetCount = 0;
-        gdErrorProperty.setValue(0.0);
-        igdErrorProperty.setValue(0.0);
-        igdPlusErrorProperty.setValue(0.0);
-        spreadErrorProperty.setValue(0.0);
+        gdErrorProperty.setValue("0.0");
+        igdErrorProperty.setValue("0.0");
+        igdPlusErrorProperty.setValue("0.0");
+        spreadErrorProperty.setValue("0.0");
+        epsilonErrorProperty.setValue("0.0");
+        hvErrorProperty.setValue("0.0");
+        erErrorProperty.setValue("0.0");
 
         gdCheckBox.setDisable(true);
         igdCheckBox.setDisable(true);
